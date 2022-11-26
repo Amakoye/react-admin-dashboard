b218f90599b0e4331b7720c99ca8d434dc83c6cb
Amakoye <charlesamakoye@gmail.com>
2022-11-27 01:06:51 +0300

    added custom Header component

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import Header from "../../components/Header";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
